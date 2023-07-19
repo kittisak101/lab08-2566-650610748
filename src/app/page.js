a4514a95d14a34eea0337acd36d1f03e286b382d
kittisak101 <kittiisak_b@cmu.ac.th>
2023-07-19 16:43:45 +0700

    change

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
         <Task title="Sleep" />
       </div>
       {/* //footer section */}
-      <Footer year="2023" fullName="Kittisak Bundit" studentId="650610748" />
+      <Footer year="2023" fullname="Kittisak Bundit" studentId="650610748" />
     </div>
   );
 }
